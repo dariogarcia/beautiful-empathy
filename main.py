a78031d3898577bc25108148bcde09a5475909e6
Dario Garcia-Gasulla <dario.garcia@bsc.es>
2020-12-13 18:47:36 +0100

    Added curses

--- main.py
+++ main.py
@@ -1,36 +1,80 @@
 import csv
+import curses
+from curses import wrapper
+import random
 
-def game_init():
-    input('Welcome to Beautiful Empathy. Press Enter to begin one round of 5 questions.')
+def read_questions(qs):
+    with open('Questions - Sheet1.tsv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='\t')
+        line_count = 0
+        for row in csv_reader:
+            qs[row[0].replace("mask","   ")] = (row[1],row[2])
+            print(qs)
+
+def game_init(stdscr):
+    stdscr.addstr(0, 0, 'Welcome to Beautiful Empathy.\n'\
+            '       Press a key to begin a new mural.'
+            , curses.color_pair(1))
+    stdscr.refresh()
+    c = stdscr.getch()
+
+def choose_color(stdscr):
+    #1
+    stdscr.addstr(0, 0, 'Player 1: Press key to select starting color at rand.'
+            , curses.color_pair(1))
+    stdscr.refresh()
+    c = stdscr.getch()
+    #2
+    rand_col_1 = random.randint(1,20)
+    stdscr.clear()
+    stdscr.addstr(0, 0, 'Player 1, your initial color is '+str(rand_col_1)
+        , curses.color_pair(1))
+    stdscr.addstr(2, 0, 'Player 2: Press key to select starting color at rand.'
+            , curses.color_pair(1))
+    c = stdscr.getch()
+    #3
+    rand_col_2 = random.randint(1,20)
+    stdscr.clear()
+    stdscr.addstr(0, 0, 'Player 1, your initial color is '+str(rand_col_1)
+        , curses.color_pair(1))
+    stdscr.addstr(2, 0, 'Player 2, your initial color is '+str(rand_col_2)
+            , curses.color_pair(1))
+    c = stdscr.getch()
+
+
+
+def main(self):
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+    stdscr = curses.initscr()
+    # Clear screen
+    stdscr.clear()
+    #No echo of keys in screen
+    curses.noecho()
+    #No enter needed to empty buffer
+    curses.cbreak()
+    #Parse keypad
+    stdscr.keypad(True)
+   
+    questions = {}
+    #Read questions file
+    read_questions(questions)
     
+    #Game init
+    game_init(stdscr)
+    stdscr.clear()
 
-#change input to keyboard    
+    #Choose first color
+    choose_color(stdscr)
+
     
-with open('Questions - Sheet1.tsv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter='\t')
-    line_count = 0
-    for row in csv_reader:
+    for round in range(5):
+        play_round
+
+    #End program
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
 
 
-        if line_count == 0:
-            game_init()
-            line_count += 1
-#count 5 rounds here!
-        else:
-#increase size
-            print(f'{row[0].replace("mask","   ")}')
-            input('Press Space for the words')
-            print(f'{row[1]}  -  {row[2]}')
-            input('Press Enter for next question')
-
-
-
-
-            line_count += 1
-    print(f'Processed {line_count} lines.')
-
-
-
-
-
-input("Press Enter to continue...")
+wrapper(main)
