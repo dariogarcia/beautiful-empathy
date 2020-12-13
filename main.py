5ca53972048f31b07e5aa83e6881e4290521bcaf
Dario Garcia-Gasulla <dario.garcia@bsc.es>
2020-12-14 00:17:08 +0100

    Playable version

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def game_init(stdscr):
 
 def choose_color(stdscr):
     #1
-    stdscr.addstr(0, 0, 'Player 1: Press key to select starting color at rand.'
+    stdscr.addstr(0, 0, 'Player 1: Press key to select starting random color.'
             , curses.color_pair(1))
     stdscr.refresh()
     c = stdscr.getch()
@@ -29,11 +29,13 @@ def choose_color(stdscr):
     stdscr.clear()
     stdscr.addstr(0, 0, 'Player 1, your initial color is '+str(rand_col_1)
         , curses.color_pair(1))
-    stdscr.addstr(2, 0, 'Player 2: Press key to select starting color at rand.'
+    stdscr.addstr(2, 0, 'Player 2: Press key to select starting random color.'
             , curses.color_pair(1))
     c = stdscr.getch()
     #3
     rand_col_2 = random.randint(1,20)
+    while rand_col_2 == rand_col_1 :
+        rand_col_2 = random.randint(1,20)
     stdscr.clear()
     stdscr.addstr(0, 0, 'Player 1, your initial color is '+str(rand_col_1)
         , curses.color_pair(1))
@@ -41,10 +43,69 @@ def choose_color(stdscr):
             , curses.color_pair(1))
     c = stdscr.getch()
 
+def play_round(stdscr, num_round, questions):
+    stdscr.addstr(0, 0, '---------------------------'
+            , curses.color_pair(3))
+    stdscr.addstr(1, 0, '---- Round number '+str(num_round+1)+'/5 -------'
+            , curses.color_pair(3))
+    stdscr.addstr(2, 0, '---------------------------'
+            , curses.color_pair(3))
+    stdscr.addstr(4, 0, '---------------------------'
+            , curses.color_pair(3))
+    stdscr.refresh()
+    read_questions = []
+    hits = 0
+    for num_question in range(5):
+        stdscr.addstr(3, 0, '---- Question number '+str(num_question+1)+'/5 ----'
+                , curses.color_pair(3))
+        stdscr.addstr(6, 0, 'Press any key for a sentence')
+        stdscr.clrtoeol()
+        stdscr.refresh()
+        stdscr.addstr(8, 0, 'Sentence: ')
+        stdscr.clrtoeol()
+        stdscr.addstr(10, 0, 'Words: ')
+        stdscr.clrtoeol()
+        stdscr.refresh()
+        c = stdscr.getch()
+        if len(read_questions) == len(questions):
+            stdscr.addstr(6, 0, 'You run out of questions. Game will end now :('
+                , curses.color_pair(2))
+            c = stdscr.getch()
+            return
+        found = False
+        while not found: 
+            question = random.choice(list(questions.keys()))
+            if questions not in read_questions:
+                found = True
+        read_questions.append(question)
+        stdscr.addstr(8, 0, question
+                , curses.color_pair(1))
+        stdscr.addstr(6, 0, 'Press any key for the two options')
+        stdscr.clrtoeol()
+        stdscr.refresh()
+        c = stdscr.getch()
+        stdscr.addstr(10, 10, questions[question][0]+' / '+questions[question][1]
+                , curses.color_pair(1))
+        stdscr.clrtoeol()
+        stdscr.refresh()
+        stdscr.addstr(6, 0, 'Press "H" if it was correctly guessed. Any other key otherwise')
+        stdscr.clrtoeol()
+        stdscr.refresh()
+        c = stdscr.getch()
+        if c == ord('H'):
+            hits+=1
+
+    stdscr.clear()
+    stdscr.addstr(6, 0, 'You guessed '+str(hits)+' out of 5.'
+                , curses.color_pair(3))
+    stdscr.addstr(7, 0, 'Hit any key for next round')
+    c = stdscr.getch()
 
 
 def main(self):
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_YELLOW)
+    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_WHITE)
     stdscr = curses.initscr()
     # Clear screen
     stdscr.clear()
@@ -58,17 +119,19 @@ def main(self):
     questions = {}
     #Read questions file
     read_questions(questions)
-    
+    num_questions = len(questions)
+
     #Game init
     game_init(stdscr)
     stdscr.clear()
 
     #Choose first color
     choose_color(stdscr)
+    stdscr.clear()
 
-    
-    for round in range(5):
-        play_round
+    #Start playing rounds
+    for round_num in range(15):
+        play_round(stdscr,round_num,questions)
 
     #End program
     curses.nocbreak()
