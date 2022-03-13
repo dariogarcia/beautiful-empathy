c8482fcf4998bd74adc7b1d3bf9932845753287c
Dario Garcia-Gasulla <dario.garcia@bsc.es>
2022-03-13 01:14:41 +0100

    push

--- src_v2/BeautifulEmpathy.py
+++ src_v2/BeautifulEmpathy.py
@@ -1,56 +1,60 @@
+from Questions import Questions
+import tempfile
 from Color_map import Color_map
 from Mosaic import Mosaic
-import tempfile
-import subprocess
-import random
-
 class Game:
 
-    def __init__(self,questions,names):
-        self.questions = questions
+    def __init__(self,names,colors):
+        self.questions = Questions
         self.path = tempfile.mkdtemp(dir='./tmp_dirs')
         self.color_map = Color_map('circle_72',self.path)
         self.mosaic = Mosaic(self.path,14,14,60)
         self.players = names
+        for name,color in zip(names,colors):
+            self.color_map.add_color(name,color)
 
-    def show_color_map(self):
-        color_map_tmp_file = self.color_map.get_updated_color_map_path()
-        color_map_viewer = subprocess.Popen(['eog', color_map_tmp_file])
-        return color_map_viewer
 
     def player_add_color(self,player_name,color_id):
             self.color_map.add_color(player_name,color_id)
 
-    def show_mosaic(self):
-        mosaic_tmp_file = self.mosaic.get_updated_mosaic_path(self.color_map.colors)
-        mosaic_viewer = subprocess.Popen(['eog', mosaic_tmp_file])
-        return mosaic_viewer
+    def num_colors_player(self,name):
+        count = 0 
+        for c_id,color in self.color_map.colors.items():
+            if color.player_name == name:
+                count+=1
+        return count
+
+    def hex_colors_player(self,name):
+        hexes = [] 
+        for c_id,color in self.color_map.colors.items():
+            if color.player_name == name:
+                hexes.append(color.hex)
+        return hexes
+
+    def gets_new_color(self, hits,owned):
+        if owned < 4:
+            return True
+        elif hits==0:
+            return False
+        else:
+            return owned < hits+4
 
     def player_neighbour_colors(self,player_name):
         colors = self.color_map.neighbour_colors(player_name)
         return colors
 
-    def get_rand_color(self):
-        found = False
-        while not found:
-            c = random.randint(1,72)
-            if self.color_map.colors[c].in_use:
-                continue
-            break
-        return c
-
     def get_rand_shape(self, hits, owned):
         if owned < 4:
             if hits < 2:
                 return {'c':2}
-            elif hits < 4:
+            elif hits < 4: 
                 return {'c':4}
             else:#4 or 5 hits
                 return {'c':6}
-        elif 3 < owned < 7
+        elif 3 < owned < 7: 
             if hits < 2:
                 return {'c':8}
-            elif hits < 4:
+            elif hits < 4: 
                 return {'c':4,'C':2}
             else:#4 or 5 hits
                 return {'c':8,'C':2}
@@ -61,10 +65,3 @@ class Game:
                 return {'c':8,'C':2}
             else:#4 or 5 hits
                 return {'c':16}
-
-    def num_colors_player(self,name):
-        count = 0
-        for c_id,color in self.color_map.colors.items():
-            if color.player_name == name:
-                count+=1
-        return count
--- src_v2/tk.py
+++ src_v2/tk.py
@@ -1,7 +1,13 @@
 import tkinter as tk
-from tkinter import ttk, StringVar, PhotoImage
+from tkinter import ttk, IntVar, StringVar, PhotoImage
 import random
 from BeautifulEmpathy import Game
+
+MAX_PLAYERS=6
+NUM_ROUNDS=10
+NAME_LEN_LIM=3
+WINDOW_WIDTH=1800
+WINDOW_HEIGHT=900
 
 class BeautifulEmpathyApp():
 
@@ -9,6 +15,8 @@ class BeautifulEmpathyApp():
         #set tk objects
         self.root = root
         self.root.title('Beautiful Empathy')
+        self.top_frame = None
+        self.top_canvas = None
         self.left_frame = None
         self.left_canvas = None
         self.right_frame = None
@@ -17,35 +25,51 @@ class BeautifulEmpathyApp():
         #set tk config
         self.configure_main_window()
         self.create_containers()
+        self.top_frame_widgets()
         self.left_frame_widgets()
         self.right_frame_widgets()
         #game vars
         self.game = None
-        self.current_window = 0
         self.num_players = None
         self.names_players = []
         self.init_colors = []
         self.current_player_name = None
+        self.current_round = None
+        self.num_hits = None
+        self.current_shapes = None 
+        #game widgets
+        self.combo_num_players = None
 
     def configure_main_window(self):
-        window_width = 1800
-        window_height = 900
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
-        center_x = int(screen_width/2 - window_width / 2)
-        center_y = int(screen_height/2 - window_height / 2)
-        self.root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
-                
+        center_x = int(screen_width/2-WINDOW_WIDTH/2)
+        center_y = int(screen_height/2-WINDOW_HEIGHT/2)
+        self.root.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{center_x}+{center_y}')
+    
     def create_containers(self):
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(1, weight=1)
-        self.left_frame = tk.Frame(self.root, bg='black', width=500, height=500)
-        self.center_frame = tk.Frame(self.root, bg='white', width=200, height=800)
+        self.top_frame = tk.Frame(self.root, bg='white', width=1600, height=200)
+        self.left_frame = tk.Frame(self.root, bg='black', width=500, height=900)
+        self.center_frame = tk.Frame(self.root, bg='white', width=200, height=900)
         self.right_frame = tk.Frame(self.root, bg='black', width=900, height=900)
-        self.left_frame.grid(row=0, column=0)
-        self.center_frame.grid(row=0, column=1)
-        self.right_frame.grid(row=0, column=2)
+        self.top_frame.grid(row=0, column=0, columnspan=3)
+        self.left_frame.grid(row=1, column=0)
+        self.center_frame.grid(row=1, column=1)
+        self.right_frame.grid(row=1, column=2)
 
+    def top_frame_widgets(self):
+        self.title_canvas = tk.Canvas(self.top_frame, width = 1000, height = 200)
+        self.title_canvas.pack(fill='both', expand = True)
+    
+    def update_top_frame_widgets(self):
+        self.title_canvas.create_text(250,50, text="BEAUTIFUL EMPATHY", fill="purple", font=('Helvetica 25 bold'))
+        self.title_canvas.pack()
+        if self.current_round != None:
+            self.title_canvas.create_text(500, 50, text="Round Number:", fill="black")
+            self.title_canvas.pack()
+    
     def left_frame_widgets(self):
         self.left_canvas = tk.Canvas(self.left_frame, width = 600, height = 600)
         self.left_canvas.pack(fill='both', expand = True)
@@ -73,35 +97,16 @@ class BeautifulEmpathyApp():
     def clear_center_frame(self):
        for widgets in self.center_frame.winfo_children():
           widgets.destroy()
-
-    def show_next_screen(self,event=None):
-        #Transition from get num players to get names
-        if  self.current_window == 0:
-            self.current_window+=1
-            self.num_players = int(event.widget.get())
-            self.names_players = [None]*self.num_players
-            self.clear_center_frame()
-            self.get_name_players()
-            return
-        #Transition from get names to choose initial colors
-        if  self.current_window == 1:
-            for name in self.names_players:
-                if len(name.get())<1:
-                    print(name.get())
-                    return
-            self.current_window+=1
-            self.clear_center_frame()
-            self.choose_init_colors()
-            self.show_colors_players()
-            return
  
-    def limit_name_len(self, var, index, mode):
-        len_limit = 3
+    def check_names(self, var, index, mode):
         for idx, val in enumerate(self.names_players):
-            string_name = self.names_players[idx].get()[:len_limit]
+            string_name = self.names_players[idx].get()[:NAME_LEN_LIM]
             self.names_players[idx].set(string_name)
+            for prev_name in self.names_players[:idx]:
+                if string_name == prev_name.get():
+                    self.names_players[idx].set("")
 
-    def choose_init_colors(self):
+    def assign_rand_init_colors(self):
         for i in range(self.num_players):
             found = False
             while not found:
@@ -119,16 +124,29 @@ class BeautifulEmpathyApp():
    ##### SCREENS ##### 
    ###################
 
-    def get_num_players(self):
-        title = tk.Label(self.center_frame, text="Enter number of painters")
-        title.pack()
-        plist = [i+1 for i in range(6)]
-        str_num = tk.StringVar()
-        combo = ttk.Combobox(self.center_frame, values = plist)
-        combo.bind('<<ComboboxSelected>>', self.show_next_screen)
-        combo.pack(padx = 5, pady = 5)
+    def init_screen(self):
+        main_title = tk.Label(self.center_frame, text="BEAUTIFUL EMPATHY\n\
+            A game of feeling others while paiting together")
+        main_title.pack()
+        self.update_top_frame_widgets()
+        self.get_num_players()
 
-    def get_name_players(self):
+    def get_num_players(self):
+        title = tk.Label(self.center_frame, text="How many painters will"\
+            " be joining us today?")
+        title.pack()
+        plist = [i+1 for i in range(MAX_PLAYERS)]
+        self.num_players = IntVar()
+        self.combo_num_players = ttk.Combobox(self.center_frame,\
+             values = plist)
+        self.combo_num_players.bind('<<ComboboxSelected>>',\
+             self.get_name_players)
+        self.combo_num_players.pack(padx = 5, pady = 5)
+
+    def get_name_players(self,event=None):
+        self.num_players = int(self.combo_num_players.get())
+        self.clear_center_frame()
+        self.names_players = [None]*self.num_players
         warning = tk.Label(self.center_frame, text="Enter painter names\n"\
             "(3 characters MAX)")
         warning.pack()
@@ -139,75 +157,114 @@ class BeautifulEmpathyApp():
             name_entry = tk.Entry(self.center_frame, bg="white", fg="black",\
                  textvariable=self.names_players[i])
             name_entry.pack()
-            self.names_players[i].trace('w', self.limit_name_len)
+            self.names_players[i].trace('w', self.check_names)
         submit_button = tk.Button(self.center_frame, text="Next",\
-             command=self.show_next_screen)
+             command=self.show_colors_players)
         submit_button.pack()
 
     def show_colors_players(self):
-        for name,color in zip(self.names_players,self.init_colors):
-            name_label = tk.Label(self.center_frame, text="Player "+name.get()+", this is your color")
-            name_label.pack()
-            tk.Label(self.center_frame, width = 20, background=self.game.color_map.colors[color].hex).pack()
-        self.update_left_frame_widgets()
-        self.game_type_screen()
-
-    def color_chosen(self,event=None):
-        chosen_color = int(event.widget.get())
-        self.game.player_add_color(self.current_player_name)
-
-    def select_color(self, name, owned_colors, available_colors):
+        for name in self.names_players:
+            if len(name.get())<1:
+                return
         self.clear_center_frame()
-        welcome_label = tk.Label(self.center_frame, text="You win a new color.\n CONGRATULATIONS "+name+" !!!")
+        self.assign_rand_init_colors()
+        for name,color in zip(self.names_players,self.init_colors):
+            if len(name.get())<1:
+                return
+            name_label = tk.Label(self.center_frame, text="Player "+\
+                name.get()+", this is your color")
+            name_label.pack()
+            tk.Label(self.center_frame, width = 20, background=\
+                self.game.color_map.colors[color].hex).pack()
+        self.update_left_frame_widgets()
+        nx_btn = tk.Button(self.center_frame, text="Next",\
+             command=self.game_type_screen)
+        nx_btn.pack()
+    
+    def game_type_screen(self):
+        self.clear_center_frame()
+        info_label = tk.Label(self.center_frame, text="Which type of game you want to play?")
+        info_label.pack()
+        both_button = tk.Button(self.center_frame, text="Beauty & Empathy")
+        both_button.pack()
+        only_button = tk.Button(self.center_frame, text="Beauty only",\
+             command=self.start_game)
+        only_button.pack()
+
+    def start_game(self):
+        self.current_round = 0
+        self.play_new_round()
+
+    def play_new_round(self):
+        self.current_player_name = self.names_players[self.current_round]
+        self.current_round +=1
+        #Simulates empathy
+        self.num_hits = random.randint(0,5)
+        #Get num owned colors
+        num_owned_colors = self.game.num_colors_player(self.current_player_name)
+        #If deserved, offer available options
+        if(self.game.gets_new_color(self.num_hits,num_owned_colors)):
+            self.select_color()
+            return
+        self.get_shapes()
+
+    def select_color(self):
+        available_colors = self.game.player_neighbour_colors\
+        (self.current_player_name.get())
+        owned_colors = self.game.hex_colors_player(self.current_player_name.get())
+        self.clear_center_frame()
+        welcome_label = tk.Label(self.center_frame, text="You win a new color."\
+            +"\n CONGRATULATIONS "+self.current_player_name.get()+" !!!")
         welcome_label.pack()
-        info_label = tk.Label(self.center_frame, text="\nThese are the colors you own:")
+        info_label = tk.Label(self.center_frame, text=\
+            "\nThese are the colors you own:")
         info_label.pack()
         for c in owned_colors:
             color_label = tk.Label(self.center_frame, width=50, background=c)
             color_label.pack()
         info2_label = tk.Label(self.center_frame, text="\nYou can choose one of these colors:")
         info2_label.pack()
-        for idx, c in enumerate(available_colors):
-            color_label = tk.Label(self.center_frame, width=50, text=str(idx), background=self.game.color_map.colors[c].hex)
+        for c in available_colors:
+            color_label = tk.Label(self.center_frame, width=50, text=str(c), background=self.game.color_map.colors[c].hex)
             color_label.pack()
-        plist = [i for i in range(len(available_colors))]        
         chosen_color = tk.StringVar()
-        combo = ttk.Combobox(self.center_frame, values = plist)
+        combo = ttk.Combobox(self.center_frame, values = available_colors)
         combo.bind('<<ComboboxSelected>>', self.color_chosen)
         combo.pack(padx = 5, pady = 5)
+    
+    def color_chosen(self,event=None):
+        chosen_color = int(event.widget.get())
+        self.game.player_add_color(self.current_player_name.get(), chosen_color)
+        self.get_shapes()
 
+    def get_shapes(self):
+        self.clear_center_frame()
+        self.current_shapes = self.game.get_rand_shape(self.num_hits,self.game.\
+            num_colors_player(self.current_player_name.get()))
+        self.paint()
 
-    def game_type_screen(self):
-        info_label = tk.Label(self.center_frame, text="Which type of game you want to play?")
-        info_label.pack()
-        both_button = tk.Button(self.center_frame, text="Beauty & Empathy",\
-             command=self.beauty_only)
-        both_button.pack()
-        only_button = tk.Button(self.center_frame, text="Beauty only",\
-             command=self.beauty_only)
-        only_button.pack()
+    def paint(self):
+        owned_colors = self.game.hex_colors_player(self.current_player_name.get())
+        #paint
+        #info = tk.Label(self.center_frame, text="Color chosen is:")
+        #info.pack()
+        #col_label = tk.Label(self.center_frame, width=50, background=color)
+        #col_label.pack()
+        #info2 = tk.Label(self.center_frame, text="Shape chosen is:"+str(shape))
+        #info2.pack()
+        self.right_canvas.bind("<Button 1>",self.paint_square)
 
-    def beauty_only(self):
-        if self.game.played_rounds < 5:
-            for player in self.names_players:
-                self.current_player_name = player.get()
-                #Simulates empathy
-                num_hits = random.randint(0,5)
-                #Gets owned colors
-                num_owned_colors = self.game.num_colors_player(player.get())
-                #If deserved, offer available options
-                if(self.game.gets_new_color(num_hits,num_owned_colors)):
-                    available_colors = self.game.player_neighbour_colors(player.get())
-                    owned_colors = self.game.hex_colors_player(player.get())
-                    self.select_color(player.get(),owned_colors, available_colors)
-                #Get shapes
-                call_window_for_selecting_shape(player.get())
-            self.game.player_rounds+=1 
+    def paint_square(self, eventorigin):
+        global x,y
+        x = eventorigin.x
+        y = eventorigin.y
+        
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     beauty = BeautifulEmpathyApp(root)
-    beauty.get_num_players()
+    beauty.init_screen()
     beauty.create_game()
     root.mainloop()
 
